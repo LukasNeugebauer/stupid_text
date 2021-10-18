3b374f24434d9f9910e42f1872493841e9e8fb5b
Lukas Neugebauer <l.neugebauer@uke.de>
2021-10-18 17:51:35 +0200

    # for H

---
+++ nerdify.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Replace some letter with symbols and numbers
+also:
+    fuck you alina
+"""
+
+import sys
+
+
+mapping = {
+    'a': '4',
+    'A': '4',
+    'c': '(',
+    'C': '(',
+    'e': '3',
+    'E': '3',
+    'g': '9',
+    'G': '6',
+    'H': '#',
+    'i': '!',
+    'I': '1',
+    'j': '7',
+    'J': '7',
+    'l': '1',
+    'o': '0',
+    'O': '0',
+    's': '5',
+    'S': '5',
+    't': '+',
+    'z': '2',
+    'Z': '2',
+}
+mapping = str.maketrans(mapping)
+
+
+if __name__ == '__main__':
+    string = sys.argv[1]
+    print(string.translate(mapping))
